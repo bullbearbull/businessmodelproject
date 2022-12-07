63c95a429d91ef96c439a5b9340cae32e70f0aae
bullbearbull <115724211+bullbearbull@users.noreply.github.com>
2022-12-07 14:50:24 +0900

    Add files via upload

---
+++ MAIN.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon="👋",
+    page_title="나만의 요리사",
+    layout="wide")
+
+
+st.image('logo.png')
+
+st.subheader('사용에 어려움이 있으시면 근처 직원에게 도움을 요청해주세요')
+st.subheader('언제든 편하게 말씀해주세요!')
+
